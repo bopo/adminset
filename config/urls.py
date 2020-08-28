d2cf0c3ec823e0976bb852ccbaea47d97e7a7158
bopo <ibopo@126.com>
2020-08-28 18:29:55 +0800

    update commit

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,23 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-from django.conf.urls import url, include
-from config import views
-
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+from adminset.views import index
+from cmdb import asset
 
 urlpatterns = [
-    url(r'^$', views.index, name='config'),
-    url(r'^config_save/$', views.config_save, name='config_save'),
-    url(r'^token/', views.get_token, name='token'),
+    url(r'^$', index, name='index'),
+    url(r'^cmdb/', include('cmdb.urls')),
+    url(r'^navi/', include('navi.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^setup/', include('setup.urls')),
+    url(r'^monitor/', include('monitor.urls')),
+    url(r'^config/', include('config.urls')),
+    url(r'^accounts/', include('accounts.urls')),
+    url(r'^appconf/', include('appconf.urls')),
+    url(r'^delivery/', include('delivery.urls')),
+    url(r'^mfile/', include('mfile.urls')),
+    url(r'^elfinder/',include('elfinder.urls')),
+    url(r'^branches/',include('branches.urls')),
+    url(r'^webssh/(?P<ids>\d+)/$', asset.webssh, name='webssh'),
+
 ]
